d0c865812449f5cce975be8e44794971477f61e7
gengar-000 <vurabbas@gmail.com>
2025-09-19 19:34:06 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="card">
+      <h1>Hoş geldiniz</h1>
+      <p>Bu Next.js sürümü Vercel’de native çalışır.</p>
+    </main>
+  );
+}
